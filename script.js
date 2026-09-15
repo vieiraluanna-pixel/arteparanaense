55a0a575bc63348b1c266a8bf00a0357194162c2
vieiraluanna-pixel <vieira.luanna@escola.pr.gov.br>
2026-09-15 11:21:35 -0300

    alterei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const botoesCurtir = document.querySeletorAll(".curtir");
+botoesCurtir.forEach(function(botoesCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click , curtir");
+    function curtir(){
+        const contador = botaoCurtir.querySeletor("span");
+        if(curtiu === false){
+            contador.textContent++;
+            curtiu = true:}
+            else{
+                contador.textContent--;
+                curtiu = false;
+            }
+}
+});
 
